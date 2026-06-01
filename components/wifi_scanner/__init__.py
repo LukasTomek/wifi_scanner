4925444d66e07b3138256020622e3511a5c9b569
Lukas Tomek <l.tomek@kostal.com>
2026-06-01 11:40:56 +0200

    Modify init py to synchronize with cpp and h-

--- components/wifi_scanner/__init__.py
+++ components/wifi_scanner/__init__.py
@@ -3,21 +3,33 @@ import esphome.config_validation as cv
 from esphome.const import CONF_ID
 
 DEPENDENCIES = ["wifi"]
-AUTO_LOAD = []
 
 wifi_scanner_ns = cg.esphome_ns.namespace("wifi_scanner")
 WiFiScannerComponent = wifi_scanner_ns.class_(
     "WiFiScannerComponent", cg.Component
 )
 
+CONF_SCAN_DURATION = "scan_duration"
+CONF_REPORT_INTERVAL = "report_interval"
+CONF_RECONNECT_WAIT = "reconnect_wait"
+
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(WiFiScannerComponent),
-    cv.Optional("scan_interval", default="30s"): cv.time_period,
+    cv.Optional(CONF_SCAN_DURATION, default="5s"): cv.time_period,
+    cv.Optional(CONF_REPORT_INTERVAL, default="60s"): cv.time_period,
+    cv.Optional(CONF_RECONNECT_WAIT, default="10s"): cv.time_period,
 }).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-    cg.add(var.set_scan_interval(
-        config["scan_interval"].total_milliseconds
+
+    cg.add(var.set_scan_duration(
+        config[CONF_SCAN_DURATION].total_milliseconds
+    ))
+    cg.add(var.set_report_interval(
+        config[CONF_REPORT_INTERVAL].total_milliseconds
+    ))
+    cg.add(var.set_reconnect_wait(
+        config[CONF_RECONNECT_WAIT].total_milliseconds
     ))
